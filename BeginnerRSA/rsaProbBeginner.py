1642043364af4e7594b5ed73aeb3e52c3f4e046c
J0Nreynolds <j14reynolds@gmail.com>
2014-11-16 07:13:35 -0600

    Clean Up of RSA1 and RSABeginner
    Added comments and simplified things

--- BeginnerRSA/rsaProbBeginner.py
+++ BeginnerRSA/rsaProbBeginner.py
@@ -3,19 +3,29 @@ import binascii
 from Crypto.PublicKey import RSA
 import findRSAEncryptMessage
 
+#Found two 30 digit primes. Not too high, but high enough for N to be able to encrypt the plaintext.
+# Ex: 5 and 7 would not be good primes as N would be too small to encrypt the long flag we have.
 p = 521419622856657689423872613771
 q = 671998030559713968361666935769
+#N is always the mutliplication of the two primes
 n = p*q
+#Pick an e.
 e = 65537
+#Find a D with modular inverse through eGCD
 d = findRSAEncryptMessage.findDKey(e,p,q)
-print n,p,q,e,d
+#Make our flag
 flag = "RSAwithSmallNumsIsEasy"
+
+#Get the ciphertext
 c = pow(int(flag.encode('hex'),16),e,n)
 
+#Print out our variables
 print "The ciphertext is:", c 
 print "N = ", n
 print "p = ", p
 print "q = ", q
 print "e = ", e
 print "d = ", d
+
+#Compute the flag
 print binascii.unhexlify('%02x' % pow(c,d,n))
--- RSA1/rsaProb1.py
+++ RSA1/rsaProb1.py
@@ -2,31 +2,49 @@ import os
 import binascii
 from Crypto.PublicKey import RSA
 
+#Generate a random RSA object
 rsa = RSA.generate(1024,os.urandom)
+
+#Get the modulus, primes, and keys
 n = getattr(rsa, 'n')
 p = getattr(rsa, 'p')
 q = getattr(rsa, 'q')
 e = getattr(rsa, 'e')
 d = getattr(rsa, 'd')
+
+#Make a flag
 flag = "ThisIsJustTheBeginningOfRSA!"
+
+#Turn them into hex.
 c = hex(pow(int(flag.encode('hex'),16),e,n))
+#Snip the first two characters "0x" and the last "L"
+#Repeat for all variables
 c = c[:len(c)-1]
+
 n = hex(n)
 n = n[:len(n)-1]
+
 p = hex(p)
 p = p[:len(p)-1]
+
 q = hex(q)
 q = q[:len(q)-1]
+
 e = hex(e)
 e = e[:len(e)-1]
+
 d = hex(d)
 d = d[:len(d)-1]
+
+#Print out our cleaned up hex strings
 print "The ciphertext is:", c 
 print "N = ", n
 print "p = ", p
 print "q = ", q
 print "e = ", e
 print "d = ", d
+
+#Compute the flag
 c1 = int(c,16)
 d1= int(d,16)
 n1 = int(n,16)
